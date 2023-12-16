b707a029615ac36cf5545bad0e476585646dbe10
Gabrielislas1404 <gabrielislas10@hotmail.com>
2023-12-16 00:36:23 -0600

    proyecto listo para revisión

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Tripleten web_project_around
 
-# Proyecto 4 en Desarrollo Web.
+# Proyecto 4 y 5 en Desarrollo Web.
 
 ### Esta es la versión más reciente del proyecto web_project_around.
 
 💻 💻 💻
 
-#### Este es mi cuarto proyecto en el bootcamp de desarrollo web en "Tripleten". Realizado con HTML, CSS y JavaScript.
+#### Este página Web comprende hasta ahora mi 4to y 5to proyecto en el bootcamp de desarrollo web en "Tripleten". Realizado con HTML, CSS y JavaScript.
 
 ##### EL proyecto consiste de una pequeña red social a la que através de los siguientes Sprints se le irán añadiendo funciones con JavaScript para que sea totalmente funcional.
 
 - La mayor parte de esta página web fue desarrollada con Flexbox, además de ser una página responsiva, tomando como base la resolución de pantalla 320px y que se pueda adaptar a varios tamaños hasta llegar a los 1280px.
   El propósito principal de este proyecto es familiarizarme y entender los conceptos básicos de JavaScript.
-  La página contiene un ícono a la derecha del nombre del perfil, el cuál al hacer clic, abre un formulario que consiste en poner el nombre del usuario y ocupación. Los campos se actualizaran en la página al rellenar el formulario.
+
+  Las funcionalidades con JS son:
+  1.Hacer click en el botón con figura de lápiz para poder abrir y rellenar el formulario con nombre y ocupación. (Los datos se actualizan al presionar el botón "Guardar").
+  2.Hacer click en el botón rectangular con una cruz enmedio para poder abrir y rellenar el formulario que permite agregar una nueva foto con su descripción a la sección principal de la página.
+  3.Darle like a las fotos con la figura en forma de corazón
+  4.Poder abrir cada imágen en una ventana modal haciendo click en ellas.
+  5.Borrar las imágenes haciendo click en el bote de basura.
 
 ##### El proyecto ha sido realizado en VSS (Visual Studio Code) y visualizado con mi navegador Google Chrome Versión 116.0.5845.187 (Build oficial) (x86_64)
 
--- index.js
+++ index.js
@@ -6,20 +6,75 @@ const closeButton = document.querySelector(".popup__close-button");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__about-me");
 const formProfile = document.querySelector(".popup__content");
-const inputName = popupProfile.querySelector(".popup__name");
-const inputOccupation = popupProfile.querySelector(".popup__occupation");
+const inputName = popupProfile.querySelector(".popup__input_name");
+const inputOccupation = popupProfile.querySelector(".popup__input_occupation");
+//variables para llenar formulario de titulo y enlace
+const inputTitle = document.querySelector(".popup__input_title");
+const inputLink = document.querySelector(".popup__input_link");
+const pictureForm = document.querySelector(".popup__content_add");
+//variables para abrir y cerrar popup de profile__add-button
+const addButton = document.querySelector(".profile__add-button");
+const popupAdd = document.querySelector(".popup_add-button");
+const closeAddprofile = popupAdd.querySelector(".popup__close-button");
+
+//variable botón corazón
+const buttonHeart = document.querySelector(".elements__heart");
+
+//variable para imágen de elements
+const popupImage = document.querySelector(".popup__image");
+//variable para cerrar popup de imágen
+const closeImage = popupImage.querySelector(".popup__close-button");
+
+// array cards
+const initialCards = [
+  {
+    name: "Puente de Brooklyn",
+    link: "./images/brooklyn.jpg",
+  },
+  {
+    name: "Times Square",
+    link: "./images/timessquare.jpg",
+  },
+  {
+    name: "Washington D.C",
+    link: "./images/washington.jpg",
+  },
+  {
+    name: "Cloud Gate",
+    link: "./images/cloudgate.jpg",
+  },
+  {
+    name: "Miami Beach, Florida",
+    link: "./images/Miamibeach.jpg",
+  },
+  {
+    name: "Golden Gate, San Francisco",
+    link: "./images/goldengate.jpg",
+  },
+];
 
 //función para abrir y cerrar popup
 function togglePopup(popup) {
   popup.classList.toggle("popup_hide");
 }
 
+//función para cambiar color de corazón (hacer like)
+function toggleLike(buttonHeart) {
+  buttonHeart.classList.toggle("elements__black-heart");
+}
+//evento para abrir popup de profile
 buttonEdit.addEventListener("click", function () {
   togglePopup(popupProfile);
 });
 
+//evento para cerrar popup de profile
 closeButton.addEventListener("click", function () {
   togglePopup(popupProfile);
+});
+
+//evento para cerrar popup de imágen
+closeImage.addEventListener("click", function () {
+  togglePopup(popupImage);
 });
 
 //evento para cambiar nombre y ocupación
@@ -31,33 +86,24 @@ formProfile.addEventListener("submit", function (event) {
   togglePopup(popupProfile);
 });
 
-// array cards
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
+//evento para agregar imágen y su título
+pictureForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newCard = createCard(inputTitle.value, inputLink.value);
+  elements.prepend(newCard);
+  pictureForm.reset();
+  togglePopup(popupAdd);
+});
+
+//evento para abrir popup addprofile
+addButton.addEventListener("click", function () {
+  togglePopup(popupAdd);
+});
+
+//evento para cerrar popup addprofile
+closeAddprofile.addEventListener("click", function () {
+  togglePopup(popupAdd);
+});
 
 //container de cards <section class ="elements" </section>
 const elements = document.querySelector(".elements");
@@ -71,7 +117,6 @@ initialCards.forEach((item) => {
 //valores para card-template
 function createCard(name, link) {
   const cardTemplate = document.querySelector("#card-template").content;
-  //Clonar nodo
   const cardElement = cardTemplate
     .querySelector(".elements__container")
     .cloneNode(true);
@@ -86,6 +131,20 @@ function createCard(name, link) {
   const buttonDelete = cardElement.querySelector(".elements__trash");
   buttonDelete.addEventListener("click", function () {
     cardElement.remove();
+  });
+
+  // variable y evento para hacer click y abrir popup de imágen
+  const openImage = cardElement.querySelector(".elements__image");
+  openImage.addEventListener("click", function () {
+    popupImage.classList.toggle("popup_hide");
+    popupImage.querySelector("img").src = link;
+    popupImage.querySelector(".popup__card-name").textContent = name;
+  });
+
+  //variable para que corazón reaccione
+  const heartButton = cardElement.querySelector(".elements__heart");
+  heartButton.addEventListener("click", function () {
+    toggleLike(heartButton);
   });
 
   return cardElement;
